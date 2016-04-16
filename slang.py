fab0fdf5e1d1e026e79098dc9f53a4b9f46209ae
Vipul Sharma <vipul.sharma20@gmail.com>
2016-04-16 15:07:38 +0530

    add prefix/suffix slangs

---
+++ slang.py
@@ -0,0 +1,24 @@
+suffix_slang_3 = {
+        'ine': '9',
+        'aus': 'oz',
+        'ate': '8',
+        'for': '4',
+        }
+
+suffix_slang_4 = {
+        'ause': 'oz',
+        'fore': '4',
+        }
+
+general_slang = {
+        'you': 'u',
+        'for': '4',
+        }
+
+prefix_slang_3 = {
+        'for': '4'
+        }
+
+prefix_slang_4 = {
+        'fore': '4'
+        }
