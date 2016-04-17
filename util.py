1a078b9393a0bf51bc6709063be42963d5f28c4a
Vipul Sharma <vipul.sharma20@gmail.com>
2016-04-18 04:08:34 +0530

    fixed words dictionary

--- util.py
+++ util.py
@@ -3,7 +3,7 @@ Utility functions for filtering content
 """
 from nltk import tokenize
 from nltk.tokenize import word_tokenize
-
+from constants import REDUCED, SUFFIX_FLAG, PREFIX_FLAG
 
 def getWords(sentence):
     """
@@ -15,8 +15,7 @@ def getWords(sentence):
     words_dict = dict()
     for word in words:
         words_dict[word] = {}
-        words_dict[word]['lower'] = None
-        words_dict[word]['upper'] = None
-        words_dict[word]['flag'] = False
-        words_dict[word]['reduced'] = ''
-    return words_dict
+        words_dict[word][SUFFIX_FLAG] = False
+        words_dict[word][PREFIX_FLAG] = False
+        words_dict[word][REDUCED] = ''
+    return words_dict, words
