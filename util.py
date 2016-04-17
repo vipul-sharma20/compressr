281a85331d389394b112f29a8bdd268870620f10
Vipul Sharma <vipul.sharma20@gmail.com>
2016-04-17 12:51:24 +0530

    added words dictionary

--- util.py
+++ util.py
@@ -12,4 +12,11 @@ def getWords(sentence):
     :returns: list of tokens
     """
     words = word_tokenize(sentence)
-    return words
+    words_dict = dict()
+    for word in words:
+        words_dict[word] = {}
+        words_dict[word]['lower'] = None
+        words_dict[word]['upper'] = None
+        words_dict[word]['flag'] = False
+        words_dict[word]['reduced'] = ''
+    return words_dict
