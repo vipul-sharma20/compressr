16a32d2da7dc727107dbd6110b665be7d6b81d37
Vipul Sharma <vipul.sharma20@gmail.com>
2016-04-19 04:14:19 +0530

    compress as per character limit

--- compressr.py
+++ compressr.py
@@ -2,17 +2,20 @@
 
 import util
 from slang import *
-from constants import REDUCED, SUFFIX_FLAG, PREFIX_FLAG
+from constants import REDUCED, SUFFIX_FLAG, PREFIX_FLAG, LENGTH
 
 
-def reduce_suffix(words_dict, words):
+def reduce_suffix(words_dict, words, limit):
     """
     Reduce suffix from words
     :param words_dict: (dict) key-value pair of words from text
     :param words (list) word list
     :returns: reduced words dictionary
     """
+    text_length = words_dict[LENGTH]
     for word in words:
+        if text_length <= limit:
+            return words_dict
         if not words_dict[word][SUFFIX_FLAG]:
             word_length = len(word)
             if word_length > 3:
@@ -21,27 +24,27 @@ def reduce_suffix(words_dict, words):
                 if suffix_3 in suffix_slang_3:
                     words_dict[word][REDUCED] = word[:-3] + \
                             suffix_slang_3[suffix_3]
-                elif suffix_4 in suffix_slang_4:
+                    text_length -= len(suffix_slang_3[suffix_3])
+                if suffix_4 in suffix_slang_4:
                     words_dict[word][REDUCED] = word[:-4] + \
                             suffix_slang_4[suffix_4]
-                else:
-                    if not words_dict[word][PREFIX_FLAG]:
-                        words_dict[word][REDUCED] = word
-            else:
-                words_dict[word][REDUCED] = word
+                    text_length -= len(suffix_slang_4[suffix_4])
 
         words_dict[word][SUFFIX_FLAG] = True
     return words_dict
 
 
-def reduce_prefix(words_dict, words):
+def reduce_prefix(words_dict, words, limit):
     """
     Redue prefix from words
     :param words_dict: (dict) key-value pair of words from text
     :param words: (list) word list
     :returns: reduced words dictionary
     """
+    text_length = words_dict[LENGTH]
     for word in words:
+        if text_length <= limit:
+            return words_dict
         if not words_dict[word][PREFIX_FLAG]:
             word_length = len(word)
             if word_length > 3:
@@ -50,14 +53,11 @@ def reduce_prefix(words_dict, words):
                 if prefix_3 in prefix_slang_3:
                     words_dict[word][REDUCED] = prefix_slang_3[prefix_3] + \
                             word[3:]
-                elif prefix_4 in prefix_slang_4:
+                    text_length -= len(prefix_slang_3[prefix_3])
+                if prefix_4 in prefix_slang_4:
                     words_dict[word][REDUCED] = prefix_slang_4[prefix_4] + \
                             word[4:]
-                else:
-                    if not words_dict[word][SUFFIX_FLAG]:
-                        words_dict[word][REDUCED] = word
-            else:
-                words_dict[word][REDUCED] = word
+                    text_length -= len(prefix_slang_4[prefix_4])
 
         words_dict[word][PREFIX_FLAG] = True
     return words_dict
@@ -68,9 +68,11 @@ def get_text():
     Driver function; Execution starts here
     """
     text = raw_input('')
+    limit = input()
     words_dict, words = util.getWords(text)
-    words_dict = reduce_suffix(words_dict, words)
-    words_dict = reduce_prefix(words_dict, words)
+    words_sorted = sorted(words)
+    words_dict = reduce_suffix(words_dict, words_sorted, limit)
+    words_dict = reduce_prefix(words_dict, words_sorted, limit)
     for word in words:
         print words_dict[word][REDUCED],
 
