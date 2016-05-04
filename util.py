9fc8475e828ffdeb82169814e3f4be36b01145d7
Vipul Sharma <vipul.sharma20@gmail.com>
2016-05-05 01:08:58 +0530

    remove extra line

--- util.py
+++ util.py
@@ -38,7 +38,6 @@ def check_itemtype(word):
     urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', word)
     if word[0] == '#':
         return HASHTAG
-
     elif urls:
         return URL
     else:
