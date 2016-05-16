a3818c9dda979013d025be81b70cc8dc6d33edcd
Vipul Sharma <vipul.sharma20@gmail.com>
2016-05-16 22:18:00 +0530

    added few abbreviations

--- slang.py
+++ slang.py
@@ -27,3 +27,27 @@ prefix_slang_3 = {
 prefix_slang_4 = {
         'fore': '4'
         }
+
+abbreviations = {
+        "away from the keyboard": 'AFK',
+        "as soon as possible": 'ASAP',
+        "be back later": 'BBL',
+        "be back shortly": 'BBS',
+        "be right back": 'BRB',
+        "be back in a bit": 'BBIAB',
+        "be back in a few": 'BBIAF',
+        "bye bye for now": 'BBFN',
+        "by the way": 'BTW',
+        "care to chat": 'CTC',
+        "see ya": 'CYA',
+        "frequently asked questions": 'FAQ',
+        "Falling off chair laughing": 'FOCL',
+        "laughing out loud": 'LOL',
+        "for what it's worth": 'FWIW',
+        "for your information": 'FYI',
+        "i see": 'IC',
+        "in my opinion": 'IMO',
+        "in my humble opinion": 'IMHO',
+        "in other words": 'IOW',
+        "just kidding": 'J/K',
+        }
